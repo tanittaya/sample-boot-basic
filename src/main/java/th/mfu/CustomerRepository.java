ea869b7190b29d2654e36e7d9ac812b2669fe7b4
tanittaya <152164438+tanittaya@users.noreply.github.com>
2024-07-25 15:41:04 +0700

    now

--- src/main/java/th/mfu/CustomerRepository.java
+++ src/main/java/th/mfu/CustomerRepository.java
@@ -2,6 +2,7 @@ package th.mfu;
 
 import java.util.List;
 
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository  extends CrudRepository<Customer, Long>{
